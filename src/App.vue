507ad7867eac0dbf90410cc6d1316f02b96c534a
Valerio-Catellani <valeriocatellani@gmail.com>
2024-04-12 13:21:46 +0200

    reset main

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,10 @@
 <template>
-  <h1 class="text-center">Ciao</h1>
-  <i class="fa fa-solid fa-home"></i>
 </template>
 
 <script>
-  export default {
-    name: 'App'
-  }
+export default {
+  name: 'App'
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
