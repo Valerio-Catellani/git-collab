96cc3329e739454f179868918489a7a1de8e1ffa
MatteoRuffa <cortese_17@protonmail.com>
2024-04-12 10:44:44 +0200

    add header

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        prova
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HeaderComponent"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
